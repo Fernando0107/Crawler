52fbe97a043774cb88c2cd623ba2d74f9b36e4f3
Fernando González <35601479+Fernando0107@users.noreply.github.com>
2018-11-15 06:59:34 -0600

    Update
    New lexers

--- crawler.py
+++ crawler.py
@@ -1,11 +1,15 @@
 from pygments import highlight
 from pygments.lexers import JavascriptLexer
+from pygments.lexers import LiveScriptLexer
+from pygments.lexers import TypeScriptLexer
+from pygments.lexers import ObjectiveJLexer
+from pygments.lexers import CoffeeScriptLexer
 from pygments.formatters import HtmlFormatter
+
 #from pygments.lexers import guess_lexer, guess_lexer_for_filename #Guess lexers
 #from pygments.lexers import (get_lexer_by_name, get_lexer_for_filename, get_lexer_for_mimetype)  # look up
-
 #File
-filepath = 'ex.js'
+filepath = 't.js'
 
 #File open 
 fp = open(filepath) 
@@ -18,7 +22,14 @@ for i in lines:
 #print(code)
 formatter = HtmlFormatter(full=True, linenos=True)
 
-print (list(JavascriptLexer().get_tokens(code)))
+print("------------------------------------ JavascriptLexer -------------------------------\n")
+print(list(JavascriptLexer().get_tokens(code)))
+print("-------------------------------- TypeScriptLexer -----------------------------------\n")
+print(list(TypeScriptLexer().get_tokens(code)))
+print("---------------------------------- ObjectiveJLexer ---------------------------------\n")
+print(list(ObjectiveJLexer().get_tokens(code)))
+print("------------------------------------ CoffeeScriptLexer -------------------------------\n")
+print(list(CoffeeScriptLexer().get_tokens(code)))
 
 #file2 = open("save.html", "w")
 
